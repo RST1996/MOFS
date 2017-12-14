512fea1cd18128709f1dec6dba20c6f014ef006e
prajaktapatil1208 <34198085+prajaktapatil1208@users.noreply.github.com>
2017-12-14 19:29:31 +0530

    Add files via upload

--- mofs.sql
+++ mofs.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2017 at 01:53 PM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.1.7
+-- Generation Time: Dec 14, 2017 at 02:59 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.5.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -21,6 +19,76 @@ SET time_zone = "+00:00";
 --
 -- Database: `mofs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acad_form`
+--
+
+CREATE TABLE `acad_form` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `created_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `acad_form`
+--
+
+INSERT INTO `acad_form` (`id`, `name`, `description`, `status`, `created_by`) VALUES
+(1, 'Academic Feedback Form', 'This is a academic Feedback form to collect common feedback from students', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acad_form_questions`
+--
+
+CREATE TABLE `acad_form_questions` (
+  `id` int(11) NOT NULL,
+  `sub_cat_id` int(11) NOT NULL,
+  `question` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `acad_form_questions`
+--
+
+INSERT INTO `acad_form_questions` (`id`, `sub_cat_id`, `question`) VALUES
+(1, 1, 'Whether the teacher is well prepared and delivers lectures in well prepared manner?'),
+(2, 1, 'Was the back board writing clear and organised?'),
+(3, 1, 'what'),
+(4, 1, 'When'),
+(5, 1, 'what'),
+(6, 2, 'what'),
+(7, 2, 'when'),
+(8, 2, 'what'),
+(9, 2, 'what'),
+(10, 2, 'when'),
+(11, 2, 'how'),
+(12, 1, 'how');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_category`
+--
+
+CREATE TABLE `sub_category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sub_category`
+--
+
+INSERT INTO `sub_category` (`id`, `name`) VALUES
+(1, 'Theory'),
+(2, 'Practical');
 
 -- --------------------------------------------------------
 
@@ -64,8 +132,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
